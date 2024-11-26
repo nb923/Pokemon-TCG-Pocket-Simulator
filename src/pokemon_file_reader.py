0f593f306392549e3c96ecaf32867db41f2bd555
nb923 <nb923@scarletmail.rutgers.edu>
2024-11-26 11:56:38 -0500

    Add periods to end of all comments

--- src/pokemon_file_reader.py
+++ src/pokemon_file_reader.py
@@ -8,31 +8,31 @@ class PokemonFileReader:
 
     Attributes:
         types: A set of allowed types.
-        moves: A dictionary of allowed moves where the key is the name and the value a pokemon_move object
-        logger: A general logger passed from logger.py 
+        moves: A dictionary of allowed moves where the key is the name and the value a pokemon_move object.
+        logger: A general logger passed from logger.py .
     """
 
     def __init__(self):
-        """Initializes the instance with default values"""
-        
+        """Initializes the instance with default values."""
+
         self.types = set()
         self.moves = dict()
         self.logger = get_logger(__name__)
 
     def read_all_types(self):
-        """Reads allowed types from files
+        """Reads allowed types from files.
 
         Reads allowed types from pokemon_standard_types.txt and pokemon_custom_types.txt 
         and stores in self.types. 
         
         - If no standard file is provided, it does not read from
-        any file
+        any file.
         - If no custom file is provided, but a standard file exists,
-        reads only from standard
-        - If both are provided, reads from both files
+        reads only from standard.
+        - If both are provided, reads from both files.
 
         Returns:
-            A set, self.types, which stores the valid types
+            A set, self.types, which stores the valid types.
         """
 
         # Reads from pokemon_standard_types.txt
--- tests/test_pokemon_file_reader/test_read_all_types.py
+++ tests/test_pokemon_file_reader/test_read_all_types.py
@@ -10,19 +10,19 @@ def reader():
 
 @pytest.fixture
 def mock_files(monkeypatch):
-    """Returns a dynamic version of a mock for if each file exists and the contents of each file"""
+    """Returns a dynamic version of a mock for if each file exists and the contents of each file."""
 
     def _mock_files(file_exists_map, file_content_map):
-        """Creates a mock function for pathlib.Path.exists and builtins.open"""
+        """Creates a mock function for pathlib.Path.exists and builtins.open."""
         
         def mock_exists(path):
-            """Mimics pathlib.Path.exists using file_exists_map"""
+            """Mimics pathlib.Path.exists using file_exists_map."""
             return file_exists_map.get(str(path), False)
         
         monkeypatch.setattr("pathlib.Path.exists", mock_exists)
 
         def mock_open(path, mode="r"):
-            """Mimics builtins.open by using file_content_map"""
+            """Mimics builtins.open by using file_content_map."""
             file_path = str(path)
 
             if file_path in file_content_map:
@@ -42,7 +42,7 @@ def mock_files(monkeypatch):
     return _mock_files
 
 def test_both_files_exist_both_have_text(reader, mock_files, caplog):
-    """Test if both files exist and have content"""
+    """Test if both files exist and have content."""
 
     file_exists_map = {
         "pokemon_standard_types.txt": True,
@@ -94,7 +94,7 @@ Sound
     assert "Imported custom types from file" in caplog.text
 
 def test_both_files_exist_only_standard_has_text(reader, mock_files, caplog):
-    """Test if both files exist but only standard has content"""
+    """Test if both files exist but only standard has content."""
 
     file_exists_map = {
         "pokemon_standard_types.txt": True,
@@ -140,7 +140,7 @@ Colorless
     assert "Imported custom types from file" in caplog.text
 
 def test_both_files_exist_only_custom_has_text(reader, mock_files, caplog):
-    """Test if both files exist but only custom has content"""
+    """Test if both files exist but only custom has content."""
 
     file_exists_map = {
         "pokemon_standard_types.txt": True,
@@ -172,7 +172,7 @@ Sound
     assert "Imported custom types from file" in caplog.text
 
 def test_both_files_exist_both_are_empty(reader, mock_files, caplog):
-    """Test if both files exist but neighter have content"""
+    """Test if both files exist but neighter have content."""
 
     file_exists_map = {
         "pokemon_standard_types.txt": True,
@@ -197,7 +197,7 @@ def test_both_files_exist_both_are_empty(reader, mock_files, caplog):
     assert "Imported custom types from file" in caplog.text
 
 def test_no_custom_file_standard_has_text(reader, mock_files, caplog):
-    """Test if only standard file exists and has content"""
+    """Test if only standard file exists and has content."""
 
     file_exists_map = {
         "pokemon_standard_types.txt": True,
@@ -242,7 +242,7 @@ Colorless
     assert "Cannot locate custom types file, pokemon_custom_types.txt, did not import any custom types" in caplog.text
 
 def test_no_custom_file_standard_is_empty(reader, mock_files, caplog):
-    """Test if only standard file exists and has no content"""
+    """Test if only standard file exists and has no content."""
 
     file_exists_map = {
         "pokemon_standard_types.txt": True,
@@ -266,7 +266,7 @@ def test_no_custom_file_standard_is_empty(reader, mock_files, caplog):
     assert "Cannot locate custom types file, pokemon_custom_types.txt, did not import any custom types" in caplog.text
 
 def test_no_standard_file_custom_has_text(reader, mock_files, caplog):
-    """Test if only custom file exists and has content"""
+    """Test if only custom file exists and has content."""
 
     file_exists_map = {
         "pokemon_standard_types.txt": False,
@@ -292,7 +292,7 @@ Sound
     assert "Cannot locate standard types file, pokemon_standard_types.txt, did not import any types" in caplog.text
 
 def test_no_standard_file_custom_is_empty(reader, mock_files, caplog):
-    """Test if only custom file exists and has no content"""
+    """Test if only custom file exists and has no content."""
 
     file_exists_map = {
         "pokemon_standard_types.txt": False,
@@ -315,7 +315,7 @@ def test_no_standard_file_custom_is_empty(reader, mock_files, caplog):
     assert "Cannot locate standard types file, pokemon_standard_types.txt, did not import any types" in caplog.text
 
 def test_both_files_do_not_exist(reader, mock_files, caplog):
-    """Test if neither file exists"""
+    """Test if neither file exists."""
 
     file_exists_map = {
         "pokemon_standard_types.txt": False,
