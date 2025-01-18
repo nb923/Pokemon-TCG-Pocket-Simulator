a83adf80e38ba197a00ed29b090a9a61e1f10359
nb923 <nb923@scarletmail.rutgers.edu>
2025-01-18 16:26:20 -0500

    Add necessary fields for pokemon cards into pokemon_card.py

--- src/pokemon_card.py
+++ src/pokemon_card.py
@@ -1,15 +1,16 @@
 class PokemonCard:
     def __init__(self):
-        self.card_type = None
+        self.id = None
         self.name = None
-
-        self.pre_evo = None
-        self.evo_ready = None
-
+        self.stage = None
+        self.evolves_from = None
         self.health = None
         self.type = None
-        self.weaknesses = None
-        self.status = None
-
-        self.abilties = []
+        self.modifier = None
+        self.weakness = []
+        self.retreat_cost = []
+        self.abilities = []
         self.moves = []
+
+        self.states = []
+        self.evo_ready = None
